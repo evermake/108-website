f623169aa8b4e481e291a366a57156424e057244
evermake <vladeryabkin@gmail.com>
2024-07-18 11:03:35 +0300

    feat: set website favicon and dynamic `theme-color`
    Closes #1

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,17 @@
 <script lang="ts" setup>
+const colorMode = useColorMode()
+
 useHead({
   titleTemplate: t => !t ? 'InNoHassle' : `${t} • InNoHassle`,
+  link: [
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+    { rel: 'icon', type: 'image/svg+xml', sizes: 'any', href: '/favicon.svg' },
+  ],
+  meta: () => ([
+    { name: 'theme-color', content: colorMode.value === 'dark' ? '#171717' : '#ffffff' },
+  ]),
 })
 </script>
 
